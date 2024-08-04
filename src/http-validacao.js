42930f5a0277c3f341eea8a1a9b18c777e25de41
Valéria <valeria777valeria@hotmail.com>
2024-08-04 12:58:26 -0300

    adicionando validacao com fetch

---
+++ src/http-validacao.js
@@ -0,0 +1,27 @@
+function extraiLinks (arrLinks){
+    return arrLinks.map ((objetoLink) => Object.values(objetoLink).join());
+}
+
+async function checaStatus(listaURLs){
+    const arrStatus = await Promise
+    .all(  
+    listaURLs.map(async(url)=>{
+        const response = await fetch(url)
+        return response.status;
+    })
+)
+return arrStatus;
+}
+
+export default async function listaValidada(listaDeLinks){
+    const links = extraiLinks(listaDeLinks);
+    const status = await checaStatus(links);
+    return status
+}
+
+/*const res = await fetch ('https://nodejs.org/api/documentation.json');
+if (res.ok) {
+    const data = await res.json();
+    console.log(data);
+    
+}*/
